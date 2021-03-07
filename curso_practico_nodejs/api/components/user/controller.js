4a6d34d0cec84595fcdea7906f51f16842f47320
Edinson <ev.samar.c@gmail.com>
2021-03-07 18:20:12 -0500

    rutas por usuario

---
+++ curso_practico_nodejs/api/components/user/controller.js
@@ -0,0 +1,21 @@
+const store = require('../../../store/dummy');
+
+const TABLA = 'user';
+
+module.exports = function ( injectedStore){
+
+    let store = injectedStore;
+    if ( !store )
+    {
+        store = require('../../../store/dummy');
+    }
+
+    function list()
+    {
+        return store.list(TABLA);
+    }
+
+    return {
+        list,
+    };
+}
--- curso_practico_nodejs/api/components/user/network.js
+++ curso_practico_nodejs/api/components/user/network.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const controller = require('./index');
 
 const router = express.Router();
+
 router.get ( '/', function(req, res){
 
-    response.success(req , res, 'todo correcto', 200);
+    const list = controller.list();    
+    response.success(req , res, list, 200);
+    
+});
+
+router.get ( '/:id', function(req, res){
+
+    const user = controller.get(req.params.id);    
+    response.success(req , res, user, 200);
     
 });
 
